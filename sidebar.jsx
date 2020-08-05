9e8419e04e6fb35cf4fb605f9f31f41a7f4abffd
Yu-Hsuan Ho <stanley30543@gmail.com>
2020-08-06 06:05:52 +0800

    Fixed renaming bug

--- sidebar.jsx
+++ sidebar.jsx
@@ -4,38 +4,72 @@ class Sidebar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: {
-        name: 'The super expensive guitar with some history',
-        isMint: true,
-        shippingCost: 500, // need some change
-        price: 1000000,
-      },
-      seller: {
-        shippingSpeed: '~1 day',
-        address: 'some~where~~ over the rainbow~~',
-        reviews: 5,
-        contactInfo: 'catch me if you can'
-      }
+      product: props.product,
+      seller: props.seller
     }
+  }
+
+  condition () {
+    return (
+      <div>This product is {this.state.product.condition}</div>
+    );
+  }
+
+  openToOffer () {
+    if (this.state.product.isOpenToOffers) {
+      return (
+        <div>This seller is open to offers</div>
+      )
+    }
+  }
+
+  shippingSpeed () {
+    if (this.state.seller.isQuickShipper) {
+      return (
+        <div>
+          <div>Still Shipping Quickly</div>
+          <div>This seller is shipping orders within 24 hours, on average.</div>
+        </div>
+      )
+    }
+  }
+
+  sellerRaiting () {
+    var rating = '';
+    for (var i = 0; i < this.state.seller.reviews.rating; i++){
+      rating += '*';
+    }
+    for (var i = rating.length; i < 5; i++){
+      rating += '-';
+    }
+    return (
+      <div>{rating}</div>
+    );
   }
 
   render() {
     return (<div>
       <div>{this.state.product.name}</div>
-      <div>optional: mint item</div>
-      <div>{this.state.product.shippingCost}</div>
-      <div>{this.state.product.price}</div>
+      {this.condition()}
+      <div>Shipping fee: {this.state.product.shippingFee}</div>
+      <div>${this.state.product.priceOriginal}</div>
+      <div>+Shipping</div>
       <button>Add To Cart</button>
       <div>
         <button>Make an Offer</button>
-        <button>Watch</button>
+        <button>* Watch</button>
       </div>
-      <div>{this.state.seller.shippingSpeed}</div>
+      {this.openToOffer()}
+      {this.shippingSpeed()}
       <div>Buy With Confidence</div>
+      <div>Reverb Protection has you covered. We provide a safe community for finding the gear you want</div>
       <div>
         Shipped From
+        <div>{this.state.seller.name}</div>
         <div>{this.state.seller.address}</div>
-        <div>{this.state.seller.reviews}</div>
+        {this.sellerRaiting()}
+        <div>Joined Reverb</div>
+        <div>{this.state.seller.joinedYear}</div>
       </div>
       <div>
         <button>Message Seller</button>
