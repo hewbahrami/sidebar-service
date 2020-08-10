06798b53fdb765c74caf2f436391fc03e90338c0
Yu-Hsuan Ho <stanley30543@gmail.com>
2020-08-11 04:51:02 +0800

    Remove unused content

--- index.jsx
+++ index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import axios from 'axios';
 
 import Sidebar from './sidebar.jsx';
 
@@ -13,21 +12,6 @@ class App extends React.Component {
       seller: { reviews: { rating: 0 } }
     };
   }
-
-  /* // when initializing the page
-  componentDidMount() {
-    // send a get request for the product and seller infomation
-    axios.get('http://localhost:3210/api')
-      .then((result) => {
-        this.setState({
-          product: result.data.product,
-          seller: result.data.seller
-        });
-      })
-      .catch((err) => {
-        throw err;
-      });
-  } */
 
   render() {
     return (
